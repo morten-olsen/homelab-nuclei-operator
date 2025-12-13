86e03385c28163144db6e0dce2d758fc8a2c79ef
Morten Olsen <fbtijfdq@void.black>
2025-12-13 08:08:48 +0100

    fix: missing nuclei path

--- internal/jobmanager/jobmanager.go
+++ internal/jobmanager/jobmanager.go
@@ -460,6 +460,14 @@ func (m *JobManager) buildJob(scan *nucleiv1alpha1.NucleiScan) *batchv1.Job {
 										},
 									},
 								},
+								{
+									Name:  "NUCLEI_BINARY_PATH",
+									Value: "/usr/local/bin/nuclei",
+								},
+								{
+									Name:  "NUCLEI_TEMPLATES_PATH",
+									Value: "", // Empty means use default location (~/.nuclei/templates)
+								},
 							},
 						},
 					},
